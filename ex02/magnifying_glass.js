93699785a788d5b730832f69a938fbdad7cdcc7e
Daniel Garcia <dbob486@yahoo.com>
2019-09-12 22:27:34 -0700

    lol one file is in c

---
+++ ex02/magnifying_glass.js
@@ -0,0 +1,20 @@
+if (process.argv[2]){
+	var fs = require('fs');
+	var text = fs.readFileSync(process.argv[2],'utf8')
+
+	//Replace title="*"
+	result = text.replace(/<a.*?=title="(.*?)">/g, function getSubstring(match){
+		return(match.replace(/[^title="]*".*/, function getUpper(submatch){
+			return(submatch = submatch.toUpperCase());
+		}));
+	})
+
+	//Replace words between <a></a>
+	result = result.replace(/<a.*</g, function getSubstring(match){
+		return(match.replace(/(>)(.*?)(<)/, function getUpper(submatch){
+			return(submatch = submatch.toUpperCase());
+		}));
+	})
+
+console.log(result)
+}
